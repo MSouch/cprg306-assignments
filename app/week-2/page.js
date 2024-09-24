c59a34feba09a10a3ecf3327efd72f54bec5d6ff
MSouch <msouch3@gmail.com>
2024-09-24 09:57:32 -0600

    Week 2 Assignment Submit

---
+++ app/week-2/page.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+    </main>
+  );
+}
+
+export const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Maxwell Souchereau</h1>
+      <p>
+        GitHub Repository: 
+        <a href="https://github.com/MSouch" target="_blank" rel="noopener noreferrer">
+          Msouch
+        </a>
+      </p>
+    </div>
+  );
+};
+
+// combined components function 
+export default function CombinedComponent() {
+  return (
+    <>
+      <Page />
+      <StudentInfo />
+    </>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Maxwell Souchereau</h1>
+      <p>
+        GitHub Repository: 
+        <a href="https://github.com/msouch" target="_blank" rel="noopener noreferrer">
+          Msouch
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo; 
